a0435f98b170c4edd1ca9c59fa0df2cec68ec970
ntnyq <ntnyq13@gmail.com>
2025-05-12 13:03:29 +0800

    feat!: drop CJS build, now it's ESM only

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts', 'src/themes/*.ts'],
-  format: ['cjs', 'esm'],
-  target: 'node18',
+  platform: 'browser',
+  target: ['node18', 'es2022'],
 })
